5ca5ff1384b3afcc9b550d2fac091c1c9c70b54e
Jessie Nemec <jnemec@library.wisc.edu>
2015-03-30 21:59:42 -0500

    remove auto prefex

--- gulpfile.js
+++ gulpfile.js
@@ -24,7 +24,6 @@ var gulp = require('gulp'),
 gulp.task('styles', function() {
   return gulp.src('scss/master.scss')
     .pipe(sass({ style: 'expanded', }))
-    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
     .pipe(gulp.dest('dist/styles'))
     .pipe(rename({ suffix: '.min' }))
     .pipe(minifycss())
