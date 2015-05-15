d6187e2ecb1f6a1b31779578c7fc307d25688eba
Jessie Nemec <jnemec@library.wisc.edu>
2015-05-15 11:55:26 -0500

    add thumbnail image to posts

--- content-single.php
+++ content-single.php
@@ -14,6 +14,11 @@
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
+		<?php
+			if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
+			  	the_post_thumbnail('large');
+			}
+		?>
 		<?php the_content(); ?>
 		<?php
 			wp_link_pages( array(
