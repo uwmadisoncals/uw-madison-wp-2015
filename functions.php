46eba36b69d8d63ffe3425a32a9b43336541f4cc
Al Nemec <al.nemec@gmail.com>
2015-06-11 22:18:08 -0500

    Updated TGM to make it less naaaaagggggyyyy!

--- functions.php
+++ functions.php
@@ -227,10 +227,10 @@ function my_theme_register_required_plugins() {
     $config = array(
         'default_path' => '',                      // Default absolute path to pre-packaged plugins.
         'menu'         => 'tgmpa-install-plugins', // Menu slug.
-        'has_notices'  => true,                    // Show admin notices or not.
-        'dismissable'  => false,                    // If false, a user cannot dismiss the nag message.
+        'has_notices'  => false,                    // Show admin notices or not.
+        'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
         'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
-        'is_automatic' => false,                   // Automatically activate plugins after installation or not.
+        'is_automatic' => true,                   // Automatically activate plugins after installation or not.
         'message'      => '',                      // Message to output right before the plugins table.
         'strings'      => array(
             'page_title'                      => __( 'Install Required Plugins', 'tgmpa' ),
