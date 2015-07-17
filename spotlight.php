ced6b7d775e5db7b0170a9203f7473c47782e14d
Jessie Nemec <jnemec@library.wisc.edu>
2015-07-17 13:34:57 -0500

    update blog home page

--- spotlight.php
+++ spotlight.php
@@ -1,3 +1,4 @@
+<?php if (is_front_page()){ ?>
 <section class="spotlight">
 	<h1><span class="screen-reader-text">Check out the latest news and articles.</span></h1>
 	
@@ -29,3 +30,42 @@
 	</ul>
 	
 </section>
+
+<?php } else { ?>
+<section class="spotlight_list">
+	<h1><span class="screen-reader-text">Check out the latest news and articles.</span></h1>
+	
+	<div class="row toppadding">
+	<?php $args = array( 
+           'post__not_in' => get_option( 'sticky_posts' ), 
+           'posts_per_page' => 20, 
+        ); 
+		query_posts($args); ?>
+	<ul class="spotlightList">
+	<?php while (have_posts()) : the_post(); ?>
+		<li class="row">
+			<div class="spotlightImage box span-25">
+				<?php if ( has_post_thumbnail() ) { ?>
+					  <?php the_post_thumbnail('medium'); ?>
+				<?php } 
+					else{ ?>
+						<img alt=" " src="<?php echo get_template_directory_uri(); ?>/images/default_blog_img.svg">
+					<?php }
+				?>
+			</div>
+			<div class="blog_details span-66">
+				<h2 class="home link "><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+				<div class="date"><span class="screen-reader-text">Posted on </span><?php the_date(); ?> </div>
+				<?php the_excerpt(); ?>
+			</div>
+		</li>
+        <?php endwhile; ?>
+		<?php wp_reset_query(); ?>
+		
+	</ul>
+	<?php the_posts_navigation(); ?>
+	
+	
+</section>
+	
+<?php } ?>
