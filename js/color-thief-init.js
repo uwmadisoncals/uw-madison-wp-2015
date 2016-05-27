063a12db8e0411f3b2c591416e8be02f02fa9087
Al Nemec <al.nemec@gmail.com>
2016-05-27 10:57:11 -0500

    Attempting another fix to cross origin

--- js/color-thief-init.js
+++ js/color-thief-init.js
@@ -2,18 +2,38 @@ $(window).on("load", function() {
 		
 	if($("#page").hasClass("tiledPosts")) {
 		$(".grid-item").each(function() {
+
+			var griditem = $(this);
+
 			var img = $(this).find("img");
 			img.crossOrigin = 'Anonymous';
+
+			
 			var imgC = img[0];
+			
+
+			var imgSrc = imgC.getAttribute("src");
 			
 			
 
-			var colorThief = new ColorThief();
-			var color = colorThief.getColor(imgC);
+			/*var colorThief = new ColorThief();
+			var color = colorThief.getColor(imgSrc);*/
+
+			var imgN = new Image();
+			imgN.onload = function () {
+			  var colorThief = new ColorThief();
+			  var color = colorThief.getColor(imgN);
+			  var newColor = "rgba(" + color[0] + ", " +color[1]+ ", " +color[2]+ ", 1.0)";
+
+			  $(griditem).find("h2").css("color",newColor);
+			};
+			imgN.crossOrigin = 'Anonymous';
+			imgN.src = imgSrc;
+
 			
-			var newColor = "rgba(" + color[0] + ", " +color[1]+ ", " +color[2]+ ", 1.0)";
 			
-			$(this).find("h2").css("color",newColor);
+			
+			
 		});
 	}
 	
