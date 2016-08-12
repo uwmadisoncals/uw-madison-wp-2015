1e9c639d4eac465c8beac567560879e9c50ce2dd
Jessie Nemec <jnemec@library.wisc.edu>
2016-08-12 15:46:57 -0500

    working on moving links into mobile menu

--- header.php
+++ header.php
@@ -52,7 +52,44 @@ var templateUrl = '<?php home_url(); ?>';
 <div class="searchUI" aria-hidden="true"><?php get_search_form(); ?><a href="#" class="searchClose"><svg enable-background="new 0 0 32 32" aria-label="Close Search" height="32px" version="1.1" viewBox="0 0 32 32" width="32px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g><polyline fill="none" points="   649,137.999 675,137.999 675,155.999 661,155.999  " stroke="#FFFFFF" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="2"/><polyline fill="none" points="   653,155.999 649,155.999 649,141.999  " stroke="#FFFFFF" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="2"/><polyline fill="none" points="   661,156 653,162 653,156  " stroke="#FFFFFF" stroke-linecap="round" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="2"/></g><g><path d="M11.312,12.766c0.194,0.195,0.449,0.292,0.704,0.292c0.255,0,0.51-0.097,0.704-0.292c0.389-0.389,0.389-1.02,0-1.409   L4.755,3.384c-0.389-0.389-1.019-0.389-1.408,0s-0.389,1.02,0,1.409L11.312,12.766z"/><path d="M17.407,16.048L28.652,4.793c0.389-0.389,0.389-1.02,0-1.409c-0.389-0.389-1.019-0.561-1.408-0.171L15.296,15   c0,0-0.296,0-0.296,0s0,0.345,0,0.345L3.2,27.303c-0.389,0.389-0.315,1.02,0.073,1.409c0.194,0.195,0.486,0.292,0.741,0.292   s0.528-0.097,0.722-0.292L15.99,17.458l11.249,11.255c0.194,0.195,0.452,0.292,0.706,0.292s0.511-0.097,0.705-0.292   c0.389-0.389,0.39-1.02,0.001-1.409L17.407,16.048z"/></g></svg></a><div id="ajaxResults"></div></div>
 <div class="searchResultsOverlay" aria-hidden="true"></div>
 
-<div class="mobileMenu"><h2 class="mobileTitle">Menu</h2><?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?><div class="subLevel1"></div><div class="subLevel2"></div></div>
+<div class="mobileMenu">
+	<h2 class="mobileTitle">Menu</h2>
+	<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+	<div class="subLevel1"></div>
+	<div class="subLevel2"></div>
+	
+	<h2 class="mobileTitle">Page Heading</h2>
+	<?php
+	//find all of the parents to the post
+	$current_page_ancestors = get_post_ancestors($post);
+	
+	$tmp_post = $post;
+
+	//get children of current page
+	$child_pages = get_pages('child_of='.$post->ID.'&parent='.$post->ID.'&sort_column=menu_order');
+	
+	if(!empty($child_pages)){?>			
+		<ul id="full_page_nav" class="flexbox-container">
+			<?php
+            foreach($child_pages as $post){
+            	setup_postdata($post);?>
+           			<li class="page_item page-item-<?php the_ID();?>">
+           				<a href="<?php the_permalink(); ?>">
+           					<div class="heading"><?php the_title(); ?> </div>
+						</a
+						
+                     </li>
+            <?php } ?>
+        </ul><!-- #nav_explore --> 
+	<?php 
+	}
+
+
+	// Done. restore original $post
+	$post = $tmp_post;
+?>
+
+</div>
 <div class="menuOverlay"></div>
 
 
