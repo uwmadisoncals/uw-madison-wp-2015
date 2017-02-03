d3e6e9668804789d0747102ecd51abca5b34bc84
Al Nemec <al.nemec@gmail.com>
2017-02-03 10:18:45 -0600

    Fix 4.7.1 filter bug

--- functions.php
+++ functions.php
@@ -1680,6 +1680,36 @@ add_theme_support( 'post-thumbnails' );
 
 
 
+
+
+
+/***** FIX 4.7.1 broken MIME detection *****/
+ function ignore_upload_ext($checked, $file, $filename, $mimes){
+
+	//we only need to worry if WP failed the first pass
+	if(!$checked['type']){
+		//rebuild the type info
+		$wp_filetype = wp_check_filetype( $filename, $mimes );
+		$ext = $wp_filetype['ext'];
+		$type = $wp_filetype['type'];
+		$proper_filename = $filename;
+
+		//preserve failure for non-svg images
+		if($type && 0 === strpos($type, 'image/') && $ext !== 'svg'){
+			$ext = $type = false;
+		}
+
+		//everything else gets an OK, so e.g. we've disabled the error-prone finfo-related checks WP just went through. whether or not the upload will be allowed depends on the <code>upload_mimes</code>, etc.
+
+		$checked = compact('ext','type','proper_filename');
+	}
+
+	return $checked;
+}
+add_filter('wp_check_filetype_and_ext', 'ignore_upload_ext', 10, 4);
+
+
+
 /**
  * Register and enqueue a custom stylesheet in the WordPress admin.
  */
