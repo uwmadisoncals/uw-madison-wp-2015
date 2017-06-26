b81f6b00253d2f30b0d923fa73477536bc34a1d9
Al Nemec <al.nemec@gmail.com>
2017-06-26 12:34:33 -0500

    applied some manual cache busting

--- functions.php
+++ functions.php
@@ -1803,7 +1803,7 @@ add_filter('wp_check_filetype_and_ext', 'ignore_upload_ext', 10, 4);
  * Register and enqueue a custom stylesheet in the WordPress admin.
  */
 function pageeditor_enqueue_custom_admin_style() {
-        wp_register_style( 'custom_wp_admin_css', get_template_directory_uri() . '/dist/styles/admin-styles.min.css', false, '1.0.0' );
+        wp_register_style( 'custom_wp_admin_css', get_template_directory_uri() . '/dist/styles/admin-styles-6262017.min.css', false, '1.0.0' );
         wp_enqueue_style( 'custom_wp_admin_css' );
 }
 add_action( 'admin_enqueue_scripts', 'pageeditor_enqueue_custom_admin_style' );
@@ -6040,13 +6040,13 @@ function my_jquery_enqueue() {
 function uw_madison_wp_2015_scripts() {
 	wp_enqueue_style( 'uw-madison-wp-2015-style', get_stylesheet_uri() );
 	
-	wp_enqueue_style( 'uw-madison-wp-2015-mainstyles', get_template_directory_uri().'/dist/styles/master.min.css' );
+	wp_enqueue_style( 'uw-madison-wp-2015-mainstyles', get_template_directory_uri().'/dist/styles/master-6262017.min.css' );
 	
 	
 
 	wp_enqueue_script( 'uw-madison-wp-2015-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 	
-	wp_enqueue_script( 'uw-madison-wp-2015-main', get_template_directory_uri() . '/dist/scripts/main.min.js', array(), '20150429', true );
+	wp_enqueue_script( 'uw-madison-wp-2015-main', get_template_directory_uri() . '/dist/scripts/main-6262017.min.js', array(), '20150429', true );
 
 	wp_enqueue_script( 'uw-madison-wp-2015-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
