69289c8bc3c63f46b0f8d6431ae0209e8506b359
Al Nemec <al.nemec@gmail.com>
2017-06-29 22:40:43 -0500

    applying async and other optimizations

--- functions.php
+++ functions.php
@@ -6037,6 +6037,22 @@ function my_jquery_enqueue() {
 /**
  * Enqueue scripts and styles.
  */
+
+// Async load
+function madisonwp2015_async_scripts($url)
+{
+    if ( strpos( $url, '#asyncload') === false )
+        return $url;
+    else if ( is_admin() )
+        return str_replace( '#asyncload', '', $url );
+    else
+	return str_replace( '#asyncload', '', $url )."' async='async"; 
+    }
+add_filter( 'clean_url', 'madisonwp2015_async_scripts', 11, 1 );
+
+
+
+
 function uw_madison_wp_2015_scripts() {
 	wp_enqueue_style( 'uw-madison-wp-2015-style', get_stylesheet_uri() );
 	
@@ -6044,11 +6060,11 @@ function uw_madison_wp_2015_scripts() {
 	
 	
 
-	wp_enqueue_script( 'uw-madison-wp-2015-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
+	wp_enqueue_script( 'uw-madison-wp-2015-navigation', get_template_directory_uri() . '/js/navigation.js#asyncload', array(), '20120206', true );
 	
-	wp_enqueue_script( 'uw-madison-wp-2015-main', get_template_directory_uri() . '/dist/scripts/main-6262017.min.js', array(), '20150429', true );
+	wp_enqueue_script( 'uw-madison-wp-2015-main', get_template_directory_uri() . '/dist/scripts/main-6262017.min.js#asyncload', array(), '20150429', true );
 
-	wp_enqueue_script( 'uw-madison-wp-2015-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+	wp_enqueue_script( 'uw-madison-wp-2015-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js#asyncload', array(), '20130115', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
