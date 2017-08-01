be0dca69c616d80711f15645a4ca9203732b2b45
Al Nemec <al.nemec@gmail.com>
2017-07-31 21:48:17 -0500

    moved ga javascript to inside <head> tag

--- header.php
+++ header.php
@@ -69,9 +69,21 @@ var templateUrl = '<?php home_url(); ?>';
 
 <?php } ?>
 
-</head>
+<?php $ga_tracking = get_theme_mod("uw-madison-wp-2015_ga_id");  if($ga_tracking) { ?>
 
-<body <?php body_class(); ?> style="background-color: #<?php background_color(); ?>">
+	<script>
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+	ga('create', '<?php echo $ga_tracking ?>', 'auto');
+	ga('send', 'pageview');
+
+	</script>
+
+<?php } ?>
+
 
 <?php $gcse = get_theme_mod("uw-madison-wp-2015_gcse_id"); ?>
 <?php if($gcse != "") { ?>
@@ -92,20 +104,13 @@ var templateUrl = '<?php home_url(); ?>';
 </script>
 <?php } ?>
 
-<?php $ga_tracking = get_theme_mod("uw-madison-wp-2015_ga_id");  if($ga_tracking) { ?>
+</head>
 
-	<script>
-	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+<body <?php body_class(); ?> style="background-color: #<?php background_color(); ?>">
 
-	ga('create', '<?php echo $ga_tracking ?>', 'auto');
-	ga('send', 'pageview');
 
-	</script>
 
-<?php } ?>
+
 	<div class="loadCover">
 		<div class="loadLogo"></div>
 	</div>
