8fc5804b031b40ba2021aac31e48b29c63f0cc09
Al Nemec <al.nemec@gmail.com>
2018-05-15 14:24:40 -0500

    Added tags option to remote content pull

--- page_editor_templates/wp_remote_content.php
+++ page_editor_templates/wp_remote_content.php
@@ -1,24 +1,35 @@
 <?php $url = get_sub_field('source_url');
 		$categories = get_sub_field('source_categories');
+		$tags = get_sub_field('source_tags');
 
 													$numposts = get_sub_field('number_of_posts');
 													$numposts = $numposts + 1;
-													if($categories == "") {
+													if($categories == "" && $tags == "") {
 														$requesturi = $url."/wp-json/wp/v2/posts?per_page=".$numposts;
 													} else {
-														$requesturi = $url."/wp-json/wp/v2/posts?per_page=".$numposts."&categories=".$categories;
+														if($tags != "") {
+															$tagsuri = "&tags=".$tags;
+														}
+
+														if($categories != "") {
+															$categoriesuri = "&categories=".$categories;
+														}
+
+														$requesturi = $url."/wp-json/wp/v2/posts?per_page=".$numposts.$categoriesuri.$tagsuri;
+
+
 													}
 													//$requesturi = $url."/wp-json/wp/v2/posts?per_page=".$numposts."&categories=".$categories;
 													//console.log($requesturi);
 													?>
-													
+
 
 													<div class="remoteContent" data-remoteurl="<?php echo $requesturi ?>">
-													
+
 														<div class="loadingWrapper"><div class="loading"></div></div>
-													
+
 													</div>
-													
-													
-														
-													
+
+
+
+
