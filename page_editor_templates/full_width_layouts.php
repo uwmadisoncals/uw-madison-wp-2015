5107d5a993f6bbc77c74872dc88f47441c316f1c
Al Nemec <al.nemec@gmail.com>
2018-05-16 16:13:02 -0500

    Added new beta image carousel

--- page_editor_templates/full_width_layouts.php
+++ page_editor_templates/full_width_layouts.php
@@ -1,13 +1,13 @@
 
 											<?php if(get_sub_field('row_background') == "color" || get_sub_field('row_background') == "image") { ?>
-													
-												
+
+
 
 													<?php if(get_sub_field('row_background') == "color") { ?>
 														<div style="background: <?php the_sub_field('row_background_color'); ?>">
 													<?php } else { ?>
-														
-														<?php 
+
+														<?php
 															$imageposition = get_sub_field('row_background_image_verticalpos');
 															$image = get_sub_field('row_background_image');
 															$size = 'large'; // (thumbnail, medium, large, full or custom size)
@@ -18,8 +18,8 @@
 
 															if( $image ) { ?>
 																<?php $bgposition = get_sub_field('row_background_image_position');
-																		
-																		
+
+
 																		if($bgposition) {
 																			if($bgposition == "fixed") {
 																				$bgimagestyle = 'background-attachment: fixed; ';
@@ -29,29 +29,29 @@
 																		} else {
 																			$bgimagestyle = 'background-attachment: scroll; ';
 																		}
-																
+
 																 ?>
-																
-																
+
+
 
 															<div style="background: url(<?php echo wp_get_attachment_image_url( $image, $size ); ?>) no-repeat; background-size: cover; background-position: <?php echo $imageposition; ?> center; <?php echo $bgimagestyle; ?>" >
-															
+
 															<?php }
 
 															?>
 
-														
+
 													<?php } ?>
 
-													
 
-							   										
+
+
 
 											<?php } ?>
 
 
-											 <?php $rowspacingtop = get_sub_field('row_spacing_top'); 
-																 	
+											 <?php $rowspacingtop = get_sub_field('row_spacing_top');
+
 																 	   if($rowspacingtop == true) {
 																			$rowspacingtopclass = "removetoppadding";
 																		} else {
@@ -59,8 +59,8 @@
 																		}
 																 ?>
 
-																  <?php $rowspacingbottom = get_sub_field('row_spacing_bottom'); 
-																 	
+																  <?php $rowspacingbottom = get_sub_field('row_spacing_bottom');
+
 																 	   if($rowspacingbottom == true) {
 																			$rowspacingbottomclass = "removebottompadding";
 																		} else {
@@ -69,31 +69,35 @@
 																 ?>
 
 
-																  <?php $rowwidthlimit = get_sub_field('row_width_limit'); 
-																 	
+																  <?php $rowwidthlimit = get_sub_field('row_width_limit');
+
 																 	   if($rowwidthlimit == true) {
 																			$rowwidthlimitclass = "removewidthlimit";
 																		} else {
 																			$rowwidthlimitclass = "";
 																		}
 																 ?>
-																						
-											<?php if( have_rows('full_width_content_options') ) { ?>
-												
 
-												
+											<?php if( have_rows('full_width_content_options') ) { ?>
+
+
+
 
 												<div class="flex-row-wrapper <?php echo $rowspacingtopclass ?> <?php echo $rowspacingbottomclass ?> <?php echo $rowwidthlimitclass ?>">
 												<?php while ( have_rows('full_width_content_options') ) : the_row(); ?>
-												
-												
+
+
 													<?php if( get_row_layout() == 'accordion_panel' ) { ?>
-													
+
 														<?php include 'accordion.php'; ?>
-														 
-														 
+
+													<?php } else if( get_row_layout() == 'image_carousel' ) { ?>
+
+														<?php include 'imageslider.php'; ?>
+
+
 													 <?php } else if( get_row_layout() == 'body_text' ) { ?>
-														
+
 														<?php if(get_sub_field('content_filter') == "1") {  ?>
 															<div class="contentFilterWrapper">
 																<div class="contentFilter">
@@ -110,142 +114,142 @@
 														 <?php the_sub_field("text_content"); ?>
 
 														 <?php } ?>
-														 
+
 													 <?php } else if( get_row_layout() == 'title_text' ) { ?>
 														<header class="entry-header small-header">
 														 <h1 class="entry-title <?php the_sub_field('heading_size'); ?>"><?php the_sub_field("text_content"); ?></h1>
 														</header>
-														 
+
 													 <?php } else if( get_row_layout() == 'button' ) { ?>
-															<?php $buttonlink = get_sub_field("button_action"); 
+															<?php $buttonlink = get_sub_field("button_action");
 																if($buttonlink == "page") { ?>
-																	
-																
+
+
 																<a href="<?php the_sub_field("link_to_page"); ?>" class="button blue"><?php the_sub_field("button_text"); ?></a>
 															<?php } else { ?>
 																<a href="<?php the_sub_field("button_link"); ?>" class="button blue"><?php the_sub_field("button_text"); ?></a>
 															<?php } ?>
-														 
+
 													 <?php } else if( get_row_layout() == 'events' ) { ?>
-														  
+
 														 <?php $eventsurl = get_sub_field("events_url");
 															 	$eventsnum = get_sub_field("number_of_events");
-															 	
+
 															 uwmadison_events($eventsurl, array('limit' => $eventsnum,'title' => 'Events', 'grouped' => TRUE)) ?>
 
 													<?php } else if( get_row_layout() == 'fund_raising' ) { ?>
 														  		<?php include 'fundraising.php'; ?>
-															
+
 													<?php } else if( get_row_layout() == 'social' ) { ?>
-														  
+
 																<?php include 'social.php'; ?>
 													 <?php } else if( get_row_layout() == 'photo' ) { ?>
-														
-														  <?php 
+
+														  <?php
 
 															$image = get_sub_field('photo_from_library');
 															$size = 'large'; // (thumbnail, medium, large, full or custom size)
-															
+
 															if( $image ) {
-															
+
 																echo wp_get_attachment_image( $image, $size );
-															
+
 															}
-															
+
 															?>
-				
+
 													 <?php }
 
 
 													 if( get_row_layout() == 'directory_listing' ) {
-													include 'directory_listing.php';	
+													include 'directory_listing.php';
 
 												 }
 
 												 if( get_row_layout() == 'remote_wp_content' ) {
-													include 'wp_remote_content.php';	
+													include 'wp_remote_content.php';
 
 												 }
 
 												 if( get_row_layout() == 'periodic_table' ) {
-													include 'periodic_table.php';	
+													include 'periodic_table.php';
 
 												 }
 
-												
+
 													if( get_row_layout() == 'person_feature' ) {
-														 
-															 
+
+
 															 ?>
-															 
-								    
-								    
-								    
-														 	
+
+
+
+
+
 														 	<div class="personFeaturefw">
 															 	<div class="site-content-inner">
-															 	
+
 															 	<div class="row">
 																 	<div class="span-33">
 																	 	<?php if(get_sub_field("person_photo")) {  ?>
-																	 	
-																	 		
-																	<?php 
+
+
+																	<?php
 																		$personimage = get_sub_field('person_photo');
 																		$size = 'medium'; // (thumbnail, medium, large, full or custom size)
-																	
+
 																		if( $personimage ) {
-																		
-																		 	$attachment_id = get_sub_field('person_photo'); $size = "medium"; 
+
+																		 	$attachment_id = get_sub_field('person_photo'); $size = "medium";
 																		 	$personimage = wp_get_attachment_image_src($attachment_id, $size); ?>
-																		 	<div class="personphoto" style="background: url(<?php echo $personimage[0]; ?>) no-repeat; background-size: cover; background-position: center center;"></div>	
+																		 	<div class="personphoto" style="background: url(<?php echo $personimage[0]; ?>) no-repeat; background-size: cover; background-position: center center;"></div>
 																		 <?php } ?>
-																	 	
-																		
-																		
+
+
+
 																		<?php } ?>
-																		
+
 																		<?php if(get_sub_field("quote")) {  ?>
-																	 	
+
 																		 <div class="personquote"><span>“</span><div class="mainquote"><?php the_sub_field("quote"); ?></div></div>
-																		
+
 																		<?php } ?>
 																 	</div>
-																 	
+
 																 	<div class="span-66">
 																	 	<?php if(get_sub_field("section_title")) {  ?>
 																	 	<header class="entry-header small-header">
 																		 <h1 class="entry-title personsectiontitle"><?php the_sub_field("section_title"); ?></h1>
 																		</header>
 																		<?php } ?>
-																		
+
 																		<?php if(get_sub_field("persons_name")) {  ?>
-																	 	
+
 																		 <h2><?php the_sub_field("persons_name"); ?></h2>
-																		
+
 																		<?php } ?>
-																		
+
 																		<?php if(get_sub_field("person_bio")) {  ?>
-																	 	
+
 																		 <div class="bio"><?php the_sub_field("person_bio"); ?></div>
-																		
+
 																		<?php } ?>
-																	
+
 																 	</div>
 															 	</div>
-														 	</div> 
 														 	</div>
-														 	
-				
-								    
-								    
-														 	
-														<?php 
+														 	</div>
+
+
+
+
+
+														<?php
 													}
-													
-												
-													 $offsetC = get_sub_field('appearance_options'); 
-													 
+
+
+													 $offsetC = get_sub_field('appearance_options');
+
 													 if( get_row_layout() == 'fw_highlighted_content' ) {
 														 if(have_rows("fw_highlighted_content_type")) { ?>
 														  <?php if($offsetC == '3col') { ?>
@@ -259,18 +263,18 @@
 																 <div class="grid-sizer2col"></div>
 															  <div class="gutter-sizer2col"></div>
 															 <?php } ?>
-															 	
-	
-															  
-															  
-															<?php if($offsetC == "offset") { ?>	  
-			<div class="grid-item2col grid-itemspacer stamp"></div>		
+
+
+
+
+															<?php if($offsetC == "offset") { ?>
+			<div class="grid-item2col grid-itemspacer stamp"></div>
 			<?php } ?>
 
-															
+
 
 														 	<?php while ( have_rows('fw_highlighted_content_type') ) : the_row();
-														  
+
 
 
 																if(get_row_layout() == 'fw_latest_posts') {
@@ -278,56 +282,56 @@
 																		include 'fw_3col_latest_posts.php';
 																	} else {
 																		include 'fw_latest_posts.php';
-																	}	
+																	}
 																}
-																
+
 																if(get_row_layout() == 'fw_posts_by_category') {
 																	if($offsetC == "3col") {
 																		include 'fw_3col_posts_by_category.php';
 																	} else {
 																		include 'fw_posts_by_category.php';
 																	}
-																	
-																	
+
+
 																}
-																
+
 																if(get_row_layout() == 'fw_highlighted_page') {
 																	if($offsetC == "3col") {
 																		include 'fw_3col_highlighted_page.php';
 																	} else {
-																		
+
 																		include 'fw_highlighted_page.php';
 																	}
-																	
-																																
-																	
+
+
+
 																}
 
 
 																if(get_row_layout() == 'remote_posts') {
-																	
+
 																	if($offsetC == "3col") {
-																		
+
 																		include 'highlighted_remote_posts_3col.php';
 																	} else {
-																		
+
 																		include 'highlighted_remote_posts.php';
 																	}
-																	
-																																
-																	
-																}
-															
-														    endwhile; ?>
-														    
-														 	</div>	 
-														
 
-														
+
+
+																}
+
+														    endwhile; ?>
+
+														 	</div>
+
+
+
 														<?php }
-														 
+
 													 } ?>
-													 
+
 												<?php endwhile; ?>
 												</div> <!-- closing flex-row-wrapper -->
 											<?php } ?>
---
+++ page_editor_templates/imageslider.php
@@ -0,0 +1,148 @@
+<?php  //Image Carousel Template  ?>
+
+<?php $slideoptions = get_sub_field('slide_options'); ?>
+		<?php $interval = get_sub_field('rotate_interval'); ?>
+
+    <?php if( have_rows('carousel_slide') ): ?>
+    <div class="carousel_wrapper">
+
+
+
+
+	<?php while( have_rows('carousel_slide') ): the_row();
+
+		// vars
+		$title = get_sub_field('slide_title');
+		$subtitle = get_sub_field('slide_subtitle');
+		$internallink = get_sub_field('internal_link');
+		$externallink = get_sub_field('external_link');
+		$slideimage = get_sub_field('slide_image');
+
+
+		?>
+
+		<div class="carousel_slide carousel-cell">
+
+			<div class="carousel_text">
+			<?php if($internallink) { ?>
+				<a href="<?php echo $internallink ?>" class="carousel_title"><?php echo $title; ?></a>
+			<?php } else if($externallink) { ?>
+				<a href="<?php echo $externallink ?>" class="carousel_title"><?php echo $title; ?></a>
+			<?php } else { ?>
+            	<div class="carousel_title"><?php echo $title; ?></div>
+			<?php } ?>
+
+			<div class="carousel_subtitle">
+                <?php echo $subtitle; ?>
+            </div>
+			</div>
+
+			<div class="overlay"></div>
+
+			<div class="carousel_image">
+				<?php echo wp_get_attachment_image( $slideimage, 'large' ); ?>
+			</div>
+
+		</div>
+
+	<?php endwhile; ?>
+
+	</div>
+
+
+
+<?php endif; ?>
+
+
+<style>
+
+
+		.carousel_slide {
+			width: 100%;
+
+			height: 500px;
+			overflow: hidden;
+			position: relative;
+		}
+
+		.carousel_slide .overlay {
+			position: absolute;
+			bottom: 0px;
+			height: 50%;
+			left: 0px;
+			right: 0px;
+			z-index:1;
+			/* Permalink - use to edit and share this gradient: http://colorzilla.com/gradient-editor/#000000+0,000000+100&0+0,0.65+100 */
+background: -moz-linear-gradient(top, rgba(0,0,0,0) 0%, rgba(0,0,0,0.65) 100%); /* FF3.6-15 */
+background: -webkit-linear-gradient(top, rgba(0,0,0,0) 0%,rgba(0,0,0,0.65) 100%); /* Chrome10-25,Safari5.1-6 */
+background: linear-gradient(to bottom, rgba(0,0,0,0) 0%,rgba(0,0,0,0.65) 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#00000000', endColorstr='#a6000000',GradientType=0 ); /* IE6-9 */
+
+		}
+
+		.carousel_slide .carousel_title {
+			display: block;
+			font-size: 2rem;
+			color: #fff;
+			font-family: "Verlag-Black";
+			font-weight: bold;
+			text-transform: uppercase;
+			letter-spacing: 1px;
+			margin-bottom: 0px;
+			line-height: 1;
+		}
+
+		.carousel_slide .carousel_subtitle {
+			display: block;
+			font-size: 1.5rem;
+			color: rgba(255,255,255,0.7);
+			font-family: "Verlag-Book";
+			line-height: 1;
+			letter-spacing: 1px;
+		}
+
+		.carousel_slide .carousel_text {
+			position: absolute;
+			bottom: 2rem;
+			left: 2rem;
+			right: 2rem;
+			z-index:3;
+		}
+
+		.carousel_image img {
+			object-fit: cover;
+			width: 100%;
+		}
+
+
+	</style>
+	<link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css">
+	<script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
+
+
+
+		<?php if($slideoptions[0] == "rotate") { ?>
+
+			<script>
+			var elem = document.querySelector('.carousel_wrapper');
+			var flkty = new Flickity( elem, {
+			// options
+			cellAlign: 'left',
+			contain: true,
+			wrapAround: true,
+			autoPlay: <?php echo $interval ?>000
+			});
+			</script>
+		<?php } else { ?>
+
+
+		<script>
+		var elem = document.querySelector('.carousel_wrapper');
+		var flkty = new Flickity( elem, {
+		// options
+		cellAlign: 'left',
+		contain: true,
+		wrapAround: true
+		});
+		</script>
+	  <?php } ?>
