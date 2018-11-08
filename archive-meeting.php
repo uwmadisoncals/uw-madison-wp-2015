f7df3df75909953f6f7e5bae5a9985ee3fc5d30c
Al Nemec <al.nemec@wisc.edu>
2018-11-08 09:49:48 -0600

    updated how meetings are displayed

--- archive-meeting.php
+++ archive-meeting.php
@@ -26,27 +26,81 @@ $posts = get_posts(array(
 
 if( $posts ): ?>
 
+<style>
+.meetings_grid {
+	display: grid;
+	grid-template-columns: 3fr 1fr;
+}
+</style>
+
 <header class="page-header">
         <h1 class="page-title">All Meetings</h1>
 			</header><!-- .page-header -->
+
+<div class="meetings_grid">
+	<div class="currentYearMeetings">
+	<h3>Current Year</h3>
 
 	<ul style="margin-left: 0px;">
 
 	<?php foreach( $posts as $post ):
 
-		setup_postdata( $post )
+		setup_postdata( $post ) ?>
+
+		<?php
+
+		$mdate = get_field("meeting_date");
+		$newmdate = substr("$mdate", 0, 4);
+
 
 		?>
-		<li style="list-style: none; margin-left: 0px; padding-left: 0px; margin-bottom: 1rem;">
-			<a href="<?php the_permalink(); ?>" style="font-size: 1.5rem;"><?php the_title(); ?></a>
-            <div><?php the_field('meeting_time'); ?></div>
 
-		</li>
+		<?php if($newmdate == date("Y")) { ?>
+			<li style="list-style: none; margin-left: 0px; padding-left: 0px; margin-bottom: 1rem;">
+				<a href="<?php the_permalink(); ?>" style="font-size: 1.5rem;"><?php the_title(); ?></a>
+				<div><?php the_field('meeting_time'); ?></div>
+
+			</li>
+
+		<?php } ?>
 
 	<?php endforeach; ?>
 
 	</ul>
 
+	</div>
+
+	<div class="meetingsidebar">
+	<?php if($newmdate == (date("Y")-1)) { ?>
+		<h3>Previous Year Meetings (<?php echo (date("Y")-1); ?>)</h3>
+		<ul style="margin-left: 0px;">
+		<?php foreach( $posts as $post ):
+
+		setup_postdata( $post ) ?>
+
+		<?php
+
+		$mdate = get_field("meeting_date");
+		$newmdate = substr("$mdate", 0, 4);
+
+
+		?>
+
+		<?php if($newmdate == (date("Y")-1)) { ?>
+			<li style="list-style: none; margin-left: 0px; padding-left: 0px; margin-bottom: 1rem;">
+				<a href="<?php the_permalink(); ?>" style="font-size: 1.5rem;"><?php the_title(); ?></a>
+				<div><?php the_field('meeting_time'); ?></div>
+
+			</li>
+
+		<?php } ?>
+
+		<?php endforeach; ?>
+
+		</ul>
+		<?php } ?>
+	</div>
+</div>
 	<?php wp_reset_postdata(); ?>
 
 <?php endif; ?>
