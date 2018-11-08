aecfc5a0759a456866fac23e154cef4997fb6f0a
Al Nemec <al.nemec@wisc.edu>
2018-11-08 13:07:20 -0600

    updated meeting page template

--- archive-meeting.php
+++ archive-meeting.php
@@ -30,12 +30,15 @@ if( $posts ): ?>
 .meetings_grid {
 	display: grid;
 	grid-template-columns: 3fr 1fr;
+	grid-column-gap: 2rem;
 }
 </style>
 
 <header class="page-header">
-        <h1 class="page-title">All Meetings</h1>
+        <h1 class="page-title">CALS State Specialist Meetings
+</h1>
 			</header><!-- .page-header -->
+<p>Bi-monthly meetings for all college faculty and staff with Extension appointments, to provide information on reorganization activities, highlight new colleagues and programs, coordinate Extension and outreach activities in the college, and connect people with resources to support their programming. Please find past agendas and meeting summaries on each meeting page.</p>
 
 <div class="meetings_grid">
 	<div class="currentYearMeetings">
@@ -59,6 +62,7 @@ if( $posts ): ?>
 			<li style="list-style: none; margin-left: 0px; padding-left: 0px; margin-bottom: 1rem;">
 				<a href="<?php the_permalink(); ?>" target="_blank" style="font-size: 1.5rem;"><?php the_title(); ?></a>
 				<div><?php the_field('meeting_time'); ?></div>
+				<div><?php the_field('location'); ?></div>
 
 			</li>
 
@@ -67,6 +71,37 @@ if( $posts ): ?>
 	<?php endforeach; ?>
 
 	</ul>
+
+
+	<h3 style="padding-top: 2rem;">Upcoming Year (<?php echo (date("Y")+1); ?>)</h3>
+
+<ul style="margin-left: 0px;">
+
+<?php foreach( $posts as $post ):
+
+	setup_postdata( $post ) ?>
+
+	<?php
+
+	$mdate = get_field("meeting_date");
+	$newmdate = substr("$mdate", 0, 4);
+
+
+	?>
+
+	<?php if($newmdate == (date("Y")+1)) { ?>
+		<li style="list-style: none; margin-left: 0px; padding-left: 0px; margin-bottom: 1rem;">
+			<a href="<?php the_permalink(); ?>" target="_blank" style="font-size: 1.5rem;"><?php the_title(); ?></a>
+			<div><?php the_field('meeting_time'); ?></div>
+			<div><?php the_field('location'); ?></div>
+
+		</li>
+
+	<?php } ?>
+
+<?php endforeach; ?>
+
+</ul>
 
 	</div>
 
@@ -89,7 +124,7 @@ if( $posts ): ?>
 		<?php if($newmdate == (date("Y")-1)) { ?>
 			<li style="list-style: none; margin-left: 0px; padding-left: 0px; margin-bottom: 1rem;">
 				<a href="<?php the_permalink(); ?>" target="_blank" style="font-size: 1.5rem;"><?php the_title(); ?></a>
-				<div><?php the_field('meeting_time'); ?></div>
+
 
 			</li>
 
