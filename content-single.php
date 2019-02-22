77c1da8f0dc1f6783e7f09750d130f5471998d5f
Al Nemec <al.nemec@wisc.edu>
2019-02-22 14:35:08 -0600

    testing issue labels for grow

--- content-single.php
+++ content-single.php
@@ -8,8 +8,42 @@
 	  $metatitle = get_field('meta_title');
 	  $subtitleplacement = get_theme_mod('uw-madison-wp-2015_post_subtitle_setting_id'); ?>
 
+
+
+<ul>
+    <?php
+        $get_parent_cats = array(
+            'parent' => '0' //get top level categories only
+        );
+
+        $all_categories = get_categories( $get_parent_cats );//get parent categories
+
+        foreach( $all_categories as $single_category ){
+
+			$slug = $single_category->slug;
+
+			if($slug == "issues") {
+
+            //for each category, get the ID
+            $catID = $single_category->cat_ID;
+
+            //echo '<li><a href=" ' . get_category_link( $catID ) . ' ">' . $single_category->name . '</a>'; //category name & link
+            $get_children_cats = array(
+                'child_of' => $catID //get children of this parent using the catID variable from earlier
+            );
+
+			$child_cats = get_categories( $get_children_cats );//get children of parent category
+
+			$issuecat = $child_cats[0]->name;
+
+
+			}
+        } //end of categories logic ?>
+</ul>
+
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
+	<div class="deptname"><span class="issue"><?php echo $issuecat; ?></span> | <span class="dept"><?php the_field("deptname"); ?></span></div>
 	<?php if($metatitle) { ?>
 		<h3 class="metatitle"><?php echo $metatitle; ?></h3>
 		<style>
